67ba6056980ea73f39492bd6a9a70b7aa90939c5
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-03-27 19:02:25 -0400

    Added my Assignment 7 files

---
+++ WEB303As07_Utkarsh_Patel_0785541/js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+        var $content = $(this).clone().find('img').css({
+            marginLeft: 0,
+            marginTop: 0,
+            width: '800px',
+            height: '450px',
+            opacity: 100
+        });
+        // $content.addClass("img_");
+        //modal code goes here
+        e.preventDefault();
+        (function (someParameter) {
+            console.log(someParameter);
+            // Remove modal content from page and store in $content
+            var $content_ = $content.detach();
+            var modal = new Modal();
+            // Click handler calls open() method of modal object
+            
+            modal.open({
+                content: $content_,
+                width: 800,
+                height: 'auto',
+            });
+        })("ValuePassedIn");
+        var $h1 = `<h1>Utkarsh Patel's Photo Gallery</h1>`;
+        $('.modal').prepend($h1);
+        $h1.addClass("modalHead");
+    });
+});
+
+
